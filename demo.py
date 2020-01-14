9de5a56c4f688700daa0a6def81f2bb032dc9f4d
laura-rowe <laura.luoo@foxmail.com>
2020-01-14 22:09:09 +0800

    selection sort
    Resolve #2
    
    Signed-off-by: laura-rowe <laura.luoo@foxmail.com>

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-def foo():
-    return False
+def sort(arr):
+    i = 0
+
+    while i < len(arr):
+        j = i + 1
+
+        while j < len(arr):
+            if(arr[i] > arr[j]):
+                arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+        i += 1
--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,15 @@
 from demo import *
+import random
 
-def test_foo():
-    res = foo()
-    assert(res == True)
+def test_order():
+    for i in range(10000):
+        arr = [random.randint(10, 99) for _ in range(10)]
+        
+        sort(arr)
+
+        i = 0
+        j = len(arr) - 1
+        
+        while i < j:
+            assert(arr[i] <= arr[i + 1])
+            i += 1
